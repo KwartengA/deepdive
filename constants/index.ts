93dda1129a72919c9e2f890d5f76edbc46377d18
Andrews Kwarteng <andrewskwartengt@gmail.com>
2025-11-13 20:48:56 +0000

    add onboarding details

--- constants/index.ts
+++ constants/index.ts
@@ -1,23 +1,34 @@
+import onboarding1 from "../assets/images/onboarding1.png";
+import onboarding2 from "../assets/images/onboarding2.png";
+import onboarding3 from "../assets/images/onboarding3.png";
+
+export const images = {
+onboarding1,
+onboarding2,
+onboarding3
+}
+
+
 export const onboarding = [
     {
       id: 1,
-      title: "",
-      image:,
-      description: "",
+      title: "Unlock Your Focus",
+      image: onboarding1,
+      description: "Dive into distraction-free productivity with this app—work in focused bursts and recharge smarter",
 
     },
     {
         id: 2,
-        title: "",
-        image:,
-        description: "",
+        title: "Task Mastery Made Simple",
+        image: onboarding2,
+        description: "Create tasks, set priorities, and schedule with ease—watch your goals come alive in animated progress",
   
     },
     {
         id: 3,
-        title:"",
-        image:,
-        decription: "",
+        title:"Stay on Track Anywhere",
+        image: onboarding3,
+        description: "Search, track, and analyze your progress effortlessly—customize settings for notifications and profiles to own your flow",
   
     },
 
